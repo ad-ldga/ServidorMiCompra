46ccac20207246bb989cc624df3f788e6b638e7a
adrianldga <ladrondeguevaraad63@gmail.com>
2024-09-07 19:18:18 +0200

    Servidor creado

---
+++ api/Servidor.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+// Definir la ruta al archivo JSON
+const jsonFilePath = path.join(__dirname, '../ProductosActualizados.json');
+
+// Exportar una función handler que maneja la solicitud
+module.exports = (req, res) => {
+  // Configurar el encabezado de la respuesta para JSON
+  res.setHeader('Content-Type', 'application/json');
+
+  // Leer el archivo JSON
+  fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+    if (err) {
+      // Manejo de errores
+      res.status(500).json({ error: 'Error al leer el archivo JSON' });
+    } else {
+      // Devolver el contenido del archivo JSON
+      res.status(200).send(data);
+    }
+  });
+};
